d5e6f53bac05a26c64775de8b6934a228c836f7f
mvanzo <martin.vanzo@gmail.com>
2022-03-23 16:34:53 -0700

    first push about cards

---
+++ src/Components/AboutCard.jsx
@@ -0,0 +1,10 @@
+export default function AboutCard() {
+    return(
+        <div>
+            <div>Nate</div>
+            <div>Clair</div>
+            <div>Jon</div>
+            <div>Marty</div>
+        </div>
+    )
+}
