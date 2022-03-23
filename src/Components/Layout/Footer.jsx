788555958d353b2f323f0b36a692d5dc4210dee0
Jon Dimaculangan <jondmclngn@gmail.com>
2022-03-23 18:48:52 -0500

    footer added

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+        <>
+            <div className='footer-div'>
+                <p>Githubs: </p>
+                <a href='/'>Clair C.</a>  🏃‍♀️
+                <a href='/'>Jon D. </a> 🏃‍♂️
+                <a href='/'>Marty V. </a> 🏄‍♂️
+                <a href='/'>Nate C.</a> 🏊‍♂️
+
+            </div>
+        </>
+    )
+}
+
+export default Footer
